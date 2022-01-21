99cae0734ab5cc63c71075a329223c4d9c483c30
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-01-21 23:14:44 +0900

    created 2 classes.

--- NearNumber/NearNumber.h
+++ NearNumber/NearNumber.h
@@ -1,4 +1,17 @@
 #pragma once
-class NearNumber {
-
+#include<vector>
+#include<cstdint>
+class BigInteger {
+	bool sign = 0;
+	std::vector<uint8_t>mantissa;
+public:
+	BigInteger(void) {
+		mantissa.push_back(0);
+	}
+	bool sgn(void) { return sign; }
+	const std::vector<uint8_t>& mantissa(void) { return mantissa; }
 };
+class NearNumber {
+};
+typedef BigInteger __intn;
+typedef NearNumber ntuple;

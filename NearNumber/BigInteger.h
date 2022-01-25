8037b43eb47728323b37a41a18111289e7e33693
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-01-25 18:14:20 +0900

    ещё говорит: "loose cannon template."

--- NearNumber/BigInteger.h
+++ NearNumber/BigInteger.h
@@ -3,8 +3,6 @@
 #include<vector>
 #define MAX(a,b) ((a)>(b)?(a):(b))
 #define MIN(a,b) ((a)<(b)?(a):(b))
-#define SGN(x) ((x)>=0?intmax_t(1):-intmax_t(1))
-#define BOOLSGN(x) ((x)<0)
 #define ABS(x) ((x)>=0?(x):-(x))
 typedef std::vector<uint8_t> mantissat;
 class BigInteger {
@@ -20,14 +18,16 @@ public:
 	mantissat mantissa(void)const { return m; }
 	auto size(void)const { return m.size(); }
 	/* "Ex_xEZ" means "there exists x so that x is an integer." */
-	template<typename Ex_xEZ=uintmax_t>Ex_xEZ& operator=(Ex_xEZ& x) {
-		s = SGN(x); m.clear();
-		for (size_t u = 0; u < sizeof(Ex_xEZ); u++)m.push_back((ABS(x) >> (8 * u)) & 0xff);
+	template<typename Ex_xEZ>Ex_xEZ& operator=( Ex_xEZ& x) {
+		s = (x < 0); m.clear();
+		for (size_t t = 0; t < sizeof(Ex_xEZ); t++)m.push_back(uint8_t(ABS(x) >> (8 * t)));
 		fit(); return x;
 	}
-	template<>BigInteger& operator=<BigInteger>(BigInteger& x) { s = x.boolsgn(); m = x.mantissa(); return x; }
+	template<>BigInteger& operator=<BigInteger>( BigInteger& x) { s = x.boolsgn(); m = x.mantissa(); return x; }
 	BigInteger(void) { operator=(0); }
+	/* "Ex_xEZ" means "there exists x so that x is an integer." */
 	template<typename Ex_xEZ>BigInteger(Ex_xEZ& x) { operator=(x); }
+	/* "Ex_xEZ" means "there exists x so that x is an integer." */
 	template<typename Ex_xEZ>operator Ex_xEZ()const {
 		Ex_xEZ x = 0;
 		for (size_t t = 0; t < MIN(m.size(), sizeof(Ex_xEZ)); t++)x |= (Ex_xEZ(m[t]) << (8 * t));

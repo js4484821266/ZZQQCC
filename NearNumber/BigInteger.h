3b225be48524d47386e268a4a4f87929789a00e4
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-01-27 15:13:49 +0900

    added surplus margin.

--- NearNumber/BigInteger.h
+++ NearNumber/BigInteger.h
@@ -7,7 +7,9 @@
 typedef std::vector<uint8_t> mantissat;
 class BigInteger {
 	bool s = 0;
+
 	mantissat m;
+
 	void fit(void) {
 		while (m.size())
 			if (!m.back())
@@ -15,24 +17,30 @@ class BigInteger {
 			else
 				break;
 	}
+
 public:
 	intmax_t sgn(void) const {
 		return 1 - 2 * (intmax_t)s;
 	}
+
 	bool boolsgn(void)const {
 		return s;
 	}
+
 	const mantissat& mantissa(void)const {
 		return m;
 	}
+
 	size_t size(void)const {
 		return m.size();
 	}
+
 	BigInteger& operator=(BigInteger& x) {
 		s = x.boolsgn();
 		m = x.mantissa();
 		return x;
 	}
+
 	/* "Ex_xEZ" means "there exists x so that x is an integer." */
 	template<typename Ex_xEZ>
 	const Ex_xEZ& operator=(const Ex_xEZ& x) {
@@ -49,20 +57,25 @@ public:
 		fit();
 		return x;
 	}
+
 	BigInteger(void) {
 		operator=(0);
 	}
+
 	BigInteger(BigInteger& x) {
 		operator=(x);
 	}
+
 	/* "Ex_xEZ" means "there exists x so that x is an integer." */
 	template<typename Ex_xEZ>
 	BigInteger(Ex_xEZ& x) {
 		operator=(x);
 	}
+
 	operator char() const {
 		return(m.size() ? m[0] : 0);
 	}
+
 	operator bool() const {
 		for (
 			size_t t = 0;
@@ -73,6 +86,7 @@ public:
 				return true;
 		return false;
 	}
+
 	/* "Ex_xEZ" means "there exists x so that x is an integer." */
 	template<typename Ex_xEZ>
 	operator Ex_xEZ()const {
@@ -85,43 +99,67 @@ public:
 			x |= (Ex_xEZ(m[t]) << (8 * t));
 		return x * sgn();
 	}
+
 	bool operator!() const {
 		return !operator bool();
 	}
+
 	bool operator&&(BigInteger x)const {
 		return operator bool() && (bool)x;
 	}
+
 	/* "Ax" means "for all x so that x is an integer." */
 	template<typename Ax>
 	bool operator&&(Ax x) const {
-		return operator bool() && !!x;
+		return operator bool() && x;
 	}
+
 	bool operator||(BigInteger x) const {
 		return operator bool() || (bool)x;
 	}
+
 	/* "Ax" means "for all x so that x is an integer." */
 	template<typename Ax>
 	bool operator||(Ax x)const {
-		return operator bool() || !!x;
+		return operator bool() || x;
 	}
+
 	bool operator==(BigInteger x)const;
+
 	bool operator>(BigInteger x)const;
+
 	bool operator<(BigInteger x)const;
+
 	bool operator!=(BigInteger x)const;
+
 	bool operator>=(BigInteger x)const;
+
 	bool operator<=(BigInteger x)const;
+
 	BigInteger operator+()const;
+
 	BigInteger operator~()const;
+
 	BigInteger operator&(BigInteger x)const;
+
 	BigInteger operator|(BigInteger x)const;
+
 	BigInteger operator^(BigInteger x)const;
+
 	BigInteger operator<<(BigInteger x)const;
+
 	BigInteger operator>>(BigInteger x)const;
+
 	BigInteger operator+(BigInteger x)const;
+
 	BigInteger operator-(BigInteger x)const;
+
 	BigInteger operator*(BigInteger x)const;
+
 	BigInteger operator/(BigInteger x)const;
+
 	BigInteger operator%(BigInteger x)const;
+
 	BigInteger operator-()const;
 };
 typedef BigInteger __intn;
--- NearNumber/main.cpp
+++ NearNumber/main.cpp
@@ -2,7 +2,8 @@
 #include"NearNumber.h"
 int main(void) {
 	__intn x;
-	__intn y=x = 5000000000;
-	std::cout << (unsigned long long)x;
+	__intn y = x = (uintmax_t)1 << 63;
+	std::cout << "x = " << (unsigned long long)x << std::endl;
+	std::cout << "y = " << (unsigned long long)y << std::endl;
 	return 0;
 }

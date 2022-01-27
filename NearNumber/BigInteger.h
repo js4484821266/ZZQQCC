bfe740c12f17a71aa8c9d47561f58f8eecd60c76
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-01-27 20:18:41 +0900

    made the code looser.

--- NearNumber/BigInteger.h
+++ NearNumber/BigInteger.h
@@ -3,10 +3,14 @@
 #include<vector>
 #define MAX(a,b) ((a)>(b)?(a):(b))
 #define MIN(a,b) ((a)<(b)?(a):(b))
-#define ABS(x) ((x)>=0?(x):-(x))
-typedef std::vector<uint8_t> mantissat;
+#define SGN(x) ((x)<0?-1:1)
+#define ABS(x) ((x)*SGN(x))
 class BigInteger {
 	bool s = 0;
+
+	typedef  uint8_t digitt;
+
+	typedef std::vector<digitt> mantissat;
 
 	mantissat m;
 
@@ -41,18 +45,18 @@ public:
 		return x;
 	}
 
-	/* "Ex_xEZ" means "there exists x so that x is an integer." */
-	template<typename Ex_xEZ>
-	const Ex_xEZ& operator=(const Ex_xEZ& x) {
+	/* "ExEZ" means "there exists x which is an integer." */
+	template<typename ExEZ>
+	const ExEZ& operator=(const ExEZ& x) {
 		s = (x < 0);
 		m.clear();
 		for (
 			size_t t = 0;
-			t < sizeof(Ex_xEZ);
+			t < sizeof(ExEZ);
 			t++
 			)
 			m.push_back(
-				uint8_t(ABS(x) >> (8 * t))
+				digitt(ABS(x) >> (8 * t))
 			);
 		fit();
 		return x;
@@ -66,9 +70,9 @@ public:
 		operator=(x);
 	}
 
-	/* "Ex_xEZ" means "there exists x so that x is an integer." */
-	template<typename Ex_xEZ>
-	BigInteger(Ex_xEZ& x) {
+	/* "ExEZ" means "there exists x which is an integer." */
+	template<typename ExEZ>
+	BigInteger(ExEZ& x) {
 		operator=(x);
 	}
 
@@ -87,16 +91,16 @@ public:
 		return false;
 	}
 
-	/* "Ex_xEZ" means "there exists x so that x is an integer." */
-	template<typename Ex_xEZ>
-	operator Ex_xEZ()const {
-		Ex_xEZ x = 0;
+	/* "ExEZ" means "there exists x which is an integer." */
+	template<typename ExEZ>
+	operator ExEZ()const {
+		ExEZ x = 0;
 		for (
 			size_t t = 0;
-			t < MIN(m.size(), sizeof(Ex_xEZ));
+			t < MIN(m.size(), sizeof(ExEZ));
 			t++
 			)
-			x |= (Ex_xEZ(m[t]) << (8 * t));
+			x |= (ExEZ(m[t]) << (8 * t));
 		return x * sgn();
 	}
 
@@ -108,7 +112,7 @@ public:
 		return operator bool() && (bool)x;
 	}
 
-	/* "Ax" means "for all x so that x is an integer." */
+	/* "Ax" means "for all x." */
 	template<typename Ax>
 	bool operator&&(Ax x) const {
 		return operator bool() && x;
@@ -118,7 +122,7 @@ public:
 		return operator bool() || (bool)x;
 	}
 
-	/* "Ax" means "for all x so that x is an integer." */
+	/* "Ax" means "for all x." */
 	template<typename Ax>
 	bool operator||(Ax x)const {
 		return operator bool() || x;
@@ -126,11 +130,13 @@ public:
 
 	bool operator==(BigInteger x)const;
 
+	bool operator!=(BigInteger x)const {
+		return!operator==(x);
+	}
+
 	bool operator>(BigInteger x)const;
 
 	bool operator<(BigInteger x)const;
-
-	bool operator!=(BigInteger x)const;
 
 	bool operator>=(BigInteger x)const;
 
--- NearNumber/main.cpp
+++ NearNumber/main.cpp
@@ -3,7 +3,8 @@
 int main(void) {
 	__intn x;
 	__intn y = x = (uintmax_t)1 << 63;
-	std::cout << "x = " << (unsigned long long)x << std::endl;
-	std::cout << "y = " << (unsigned long long)y << std::endl;
+	std::cout << "x = " << std::hex << std::showbase << (unsigned long long)x << std::endl;
+	std::cout << "y = " << std::hex << std::showbase << (unsigned long long)y << std::endl;
+	std::cout << uintmax_t(-5) * -1 << std::endl;
 	return 0;
 }

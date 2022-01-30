e88139bda9f8959b26c72aa96927430d1237d369
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-01-30 19:46:28 +0900

    added some captions and tried some testing cases.

--- NearNumber/BigInteger.h
+++ NearNumber/BigInteger.h
@@ -7,11 +7,8 @@
 #define ABS(x) ((x)*SGN(x))
 class BigInteger {
 	bool s = 0;
-
 	typedef  uint8_t digitt;
-
 	typedef std::vector<digitt> mantissat;
-
 	mantissat m;
 
 	void fit(void) {
@@ -24,31 +21,47 @@ class BigInteger {
 
 public:
 
+	/* Returns arithmetic sign: -1 if negative, and 1 otherwise. */
 	intmax_t sgn(void) const {
 		return 1 - 2 * (intmax_t)s;
 	}
 
+	/* Returns boolean sign: true if negative, and false otherwise. */
 	bool boolsgn(void)const {
 		return s;
 	}
 
+	/* Returns a reference to a vector (a flexible array) of mantissa. */
 	const mantissat& mantissa(void)const {
 		return m;
 	}
 
+	/* Returns size of the mantissa vector, a flexible array. */
 	size_t size(void)const {
 		return m.size();
 	}
 
+	/* Returns a unit of the mantissa vector. */
+	const digitt& operator[](size_t index) const {
+		return m[index];
+	}
+
+	/* Returns a bit of the mantissa vector in boolean form. */
+	bool b(size_t index)const {
+		return!!(operator[](index / (8 * sizeof(digitt))) & (1 << (index % (8 * sizeof(digitt)))));
+	}
+
+	/* Copies the object into this. */
 	const BigInteger& operator=(const BigInteger& x) {
 		s = x.boolsgn();
 		m = x.mantissa();
 		return x;
 	}
 
-	/* "ExEZ" means "there exists x, an integer." */
+	/* "ExEZ" means "there exists x, an integer."
+	   Splits a general integer into units and puts them into this. */
 	template<typename ExEZ>
-	const ExEZ& operator=(const ExEZ& x) {
+	const ExEZ operator=(const ExEZ x) {
 		s = (x < 0);
 		m.clear();
 		for (
@@ -65,77 +78,90 @@ public:
 		return x;
 	}
 
+	/* Initialises this to 0. */
 	BigInteger(void) {
 		operator=(0);
 	}
 
+	/* Initialises this to a copy of an object. */
 	BigInteger(const BigInteger& x) {
 		operator=(x);
 	}
 
-	/* "ExEZ" means "there exists x, an integer." */
+	/* "ExEZ" means "there exists x, an integer."
+	   Initialises this to a general integer. */
 	template<typename ExEZ>
-	BigInteger(const ExEZ& x) {
+	BigInteger(const ExEZ x) {
 		operator=(x);
 	}
 
+	/* Casts this to a boolean. */
 	operator bool() const {
 		for (
 			size_t t = 0;
-			t < m.size();
+			t < size();
 			t++
 			)
-			if (m[t])
+			if (operator[](t))
 				return true;
 		return false;
 	}
 
-	/* "ExEZ" means "there exists x, an integer." */
+	/* "ExEZ" means "there exists x, an integer."
+	   Casts this to a general integer. */
 	template<typename ExEZ>
 	operator ExEZ()const {
 		ExEZ x = 0;
 		for (
 			size_t t = 0;
-			t < MIN(m.size() * sizeof(digitt), sizeof(ExEZ)) / sizeof(digitt);
+			t < MIN(size() * sizeof(digitt), sizeof(ExEZ)) / sizeof(digitt);
 			t++
 			)
-			x |= (ExEZ(m[t]) << (sizeof(digitt) * 8 * t));
+			x |= (ExEZ(operator[](t)) << (sizeof(digitt) * 8 * t));
 		return x * sgn();
 	}
 
+	/* Checks if 0. */
 	bool operator!() const {
 		return !operator bool();
 	}
 
-	bool operator&&(const BigInteger x)const {
+	/* Performs a logical AND comparison with an object. */
+	bool operator&&(const BigInteger& x)const {
 		return operator bool() && (bool)x;
 	}
 
-	/* "Ax" means "for all x." */
+	/* "Ax" means "for all x."
+	   Performs a logical AND comparison with a general integer. */
 	template<typename Ax>
 	bool operator&&(const Ax x) const {
 		return operator bool() && x;
 	}
 
-	bool operator||(const BigInteger x) const {
+	/* Performs a logical OR comparison with an object. */
+	bool operator||(const BigInteger& x) const {
 		return operator bool() || (bool)x;
 	}
 
-	/* "Ax" means "for all x." */
+	/* "Ax" means "for all x."
+	   Performs a logical OR comparison with a general integer. */
 	template<typename Ax>
 	bool operator||(const Ax x)const {
 		return operator bool() || x;
 	}
 
-	bool operator==(const BigInteger x)const {
+	/* Checks if this has the value equal to that of an object. */
+	bool operator==(const BigInteger& x)const {
 		return s == x.boolsgn() && m == x.mantissa();
 	}
 
-	bool operator!=(const BigInteger x)const {
+	/* Checks if this has the value NOT equal to that of an object. */
+	bool operator!=(const BigInteger& x)const {
 		return!operator==(x);
 	}
 
-	bool operator>(const BigInteger x)const {
+	/* Checks if this has a value greater than that of an object. */
+	bool operator>(const BigInteger& x)const {
 		if (s != x.boolsgn())
 			return x.boolsgn();
 		else
@@ -145,39 +171,102 @@ public:
 				size_t t = size();
 				while (t) {
 					--t;
-					if (m[t] > x.mantissa()[t])
+					if (operator[](t) > x[t])
 						return!s;
 				}
 				return s;
 			}
 	}
 
-	bool operator<(const BigInteger x)const{
+	/* Checks if this has a value less than that of an object. */
+	bool operator<(const BigInteger& x)const {
 		return !operator>(x) && !operator==(x);
 	}
 
-	bool operator>=(const BigInteger x)const {
+	/* Checks if this has a value greater than or equal to that of an object. */
+	bool operator>=(const BigInteger& x)const {
 		return!operator<(x);
 	}
 
-	bool operator<=(const BigInteger x)const {
+	/* Checks if this has a value less than or equal to that of an object. */
+	bool operator<=(const BigInteger& x)const {
 		return !operator>(x);
 	}
 
-	BigInteger operator<<(const BigInteger x)const;
+	/* Takes the value of bitwise AND operation. */
+	const BigInteger& operator&=(const BigInteger& x) {
+		s = s && x.boolsgn();
+		for (
+			size_t t = 0;
+			t < MIN(size(), x.size());
+			t++
+			)
+			m[t] &= x[t];
+		if (size() > x.size())
+			m.resize(x.size());
+		return*this;
+	}
+	/* Returns the value of bitwise AND operation. */
+	const BigInteger operator&(const BigInteger& x) {
+		BigInteger t = *this;
+		return t &= x;
+	}
 
-	BigInteger operator>>(const BigInteger x)const;
+	/* Takes the value of bitwise OR operation. */
+	const BigInteger& operator|=(const BigInteger& x) {
+		s = s || x.boolsgn();
+		for (
+			size_t t = 0;
+			t < MIN(size(), x.size());
+			t++
+			)
+			m[t] |= x[t];
+		if (size() < x.size())
+			for (
+				size_t t = MIN(size(), x.size());
+				t < MAX(size(), x.size());
+				t++
+				)
+				m.push_back(x[t]);
+		return*this;
+	}
+	/* Returns the value of bitwise OR operation. */
+	const BigInteger operator|(const BigInteger& x) {
+		BigInteger t = *this;
+		return t |= x;
+	}
 
-	BigInteger operator+(const BigInteger x)const;
+	/* Takes the value of bitwise OR operation. */
+	const BigInteger& operator^=(const BigInteger& x) {
+		s = s != x.boolsgn();
+		for (
+			size_t t = 0;
+			t < MIN(size(), x.size());
+			t++
+			)
+			m[t] ^= x[t];
+		if (size() < x.size())
+			for (
+				size_t t = MIN(size(), x.size());
+				t < MAX(size(), x.size());
+				t++
+				)
+				m.push_back(x[t]);
+		return*this;
+	}
+	/* Returns the value of bitwise OR operation. */
+	const BigInteger operator^(const BigInteger& x) {
+		BigInteger t = *this;
+		return t ^= x;
+	}
 
-	BigInteger operator-(const BigInteger x)const;
-
-	BigInteger operator*(const BigInteger x)const;
-
-	BigInteger operator/(const BigInteger x)const;
-
-	BigInteger operator%(const BigInteger x)const;
-
-	BigInteger operator-()const;
+	/* Takes the value whose bits are shifted. */
+	const BigInteger& operator<<=(const BigInteger& x) {
+		if (!x);
+		else if (x.boolsgn()) {
+		}
+		else;
+		return*this;
+	}
 };
 typedef BigInteger __intn;
--- NearNumber/main.cpp
+++ NearNumber/main.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
 #include"NearNumber.h"
 int main(void) {
-	__intn x(5000000000);
-	__intn y(5000000000);
-	std::cout << (y==x) << std::endl;
+	__intn x(-0);
+	__intn y(-5000000001);
+	std::cout <<
+		"x: " << (intmax_t)x <<
+		", y: " << (intmax_t)y << std::endl <<
+		"x.sgn: " << x.sgn() <<
+		", y.sgn: " << y.sgn() << std::endl <<
+		"x.boolsgn: " << x.boolsgn() <<
+		", y.boolsgn: " << y.boolsgn() << std::endl <<
+		"x.size: " << x.size() <<
+		", y.size: " << y.size() << std::endl <<
+		"x.b(0): " << x.b(0) <<
+		", y.b(0): " << y.b(0) << std::endl <<
+		"(bool)x: " << (bool)x <<
+		", (bool)y: " << (bool)y << std::endl <<
+		"!x: " << !x <<
+		", !y: " << !y << std::endl <<
+		"y&&x: " << (y && x) << std::endl <<
+		"y&&9: " << (y && 9) << std::endl <<
+		"y||x: " << (y || x) << std::endl <<
+		"x||0: " << (x || 0) << std::endl;
 	return 0;
 }

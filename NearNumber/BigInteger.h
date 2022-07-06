57bc983694c6b01a3b70dfa688992c1106a017c0
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-07-07 07:07:01 +0900

    implemented bitwise right shift operation.

--- NearNumber/BigInteger.h
+++ NearNumber/BigInteger.h
@@ -12,16 +12,15 @@
 3. Use their reference to save more space.
 */
 class BigInteger {
-	bool s = 0;
+	bool sign = 0;
 	typedef unsigned int unitt;
 	typedef std::vector<unitt> mantissat;
-	mantissat m;
-	const size_t ubitn;
+	mantissat mant;
 
 	void shorten(void) {
-		while (m.size() > 1)
-			if (!m.back())
-				m.pop_back();
+		while (mant.size() > 1)
+			if (!mant.back())
+				mant.pop_back();
 			else
 				break;
 	}
@@ -30,25 +29,45 @@ public:
 
 	/* Returns boolean sign: true if negative, and false otherwise. */
 	const bool& boolsgn(void) const {
-		return s;
+		return sign;
 	}
 
 	/* Returns a reference to a vector (a flexible array) of mantissa. */
 	const mantissat& mantissa(void) const {
-		return m;
+		return mant;
 	}
 
 	/* Returns size of the mantissa vector, a flexible array. */
 	const size_t size(void)const {
-		return m.size();
+		return mant.size();
+	}
+
+	/* Returns a hexadecimal string of this. */
+	const std::string _16(bool specified = false)const {
+		std::string out;
+		const size_t nx = sizeof(unitt) * 2;
+		char temp[6], * tempp = new char[nx + 1 + 2 + 1];
+		sprintf_s(tempp, nx + 1 + 2 + 1, "%s%s%X", sign ? "-" : "", specified ? "0x" : "", mant.back());
+		out += tempp;
+		for (
+			intmax_t t = size() - 2;
+			t >= 0;
+			t--
+			) {
+			sprintf_s(temp, 6, "%%0%zuX", nx);
+			sprintf_s(tempp, nx + 1, temp, mant[t]);
+			out += tempp;
+		}
+		delete[]tempp;
+		return out;
 	}
 
 	/* Returns a unit of the mantissa vector. */
 	const unitt& operator[](const size_t& index) const {
-		return m[index];
+		return mant[index];
 	}
 	unitt& operator[](const size_t& index) {
-		return m[index];
+		return mant[index];
 	}
 
 	/* Returns a bit of the mantissa vector in boolean form. */
@@ -58,39 +77,33 @@ public:
 
 	/* Initialises this to a copy of an object. */
 	BigInteger& operator=(BigInteger& x) {
-		s = x.boolsgn();
-		m = x.mantissa();
+		sign = x.boolsgn();
+		mant = x.mantissa();
 		return x;
 	}
-	BigInteger(const BigInteger& x) :
-		ubitn(sizeof(unitt) * 8),
-		s(x.boolsgn()),
-		m(x.mantissa()) {}
+	BigInteger(const BigInteger& x) { operator=(x); }
 
 	/* Initialises this to an integer. */
 	const intmax_t& operator=(const intmax_t& x) {
 		const size_t nbyte = MAX(sizeof(intmax_t) / sizeof(unitt), 1);
 		const intmax_t a = ABS(x);
-		s = x < 0;
-		m.clear();
+		const auto b = sizeof(unitt) * 8;
+		sign = x < 0;
+		mant.clear();
 		for (
 			size_t t = 0;
 			t < nbyte;
 			t++
 			)
-			m.push_back(
+			mant.push_back(
 				unitt(
-					a >> (ubitn * t)
+					a >> (b * t)
 				)
 			);
 		shorten();
 		return x;
 	}
-	BigInteger(const intmax_t& x = 0) :
-		ubitn(sizeof(unitt) * 8),
-		s(x < 0) {
-		operator=(x);
-	}
+	BigInteger(const intmax_t& x = 0) { operator=(x); }
 
 	/* Casts this to a boolean. */
 	operator bool() const {
@@ -107,7 +120,7 @@ public:
 
 	/* Returns arithmetic sign: -1 if negative, 1 if positive, and 0 otherwise. */
 	const intmax_t sgn(void) const {
-		return (1 - 2 * (intmax_t)s) * operator bool();
+		return (1 - 2 * (intmax_t)sign) * operator bool();
 	}
 
 	/* "xEZ" means "x is an arbitrary integer."
@@ -115,13 +128,14 @@ public:
 	template<typename xEZ>
 	operator xEZ() const {
 		xEZ x = 0;
-		const auto minn = MAX(MIN(size() * sizeof(unitt), sizeof(xEZ)) / sizeof(unitt), 1);
+		const auto minn = MAX(MIN(size() * sizeof(unitt), sizeof(xEZ)) / sizeof(unitt), 1)
+			, b = sizeof(unitt) * 8;
 		for (
 			size_t t = 0;
 			t < minn;
 			t++
 			)
-			x |= (xEZ(operator[](t)) << (ubitn * t));
+			x |= (xEZ(operator[](t)) << (b * t));
 		return xEZ(x * sgn());
 	}
 
@@ -142,7 +156,7 @@ public:
 
 	/* Checks if this has the value equal to that of an object. */
 	const bool operator==(const BigInteger& x)const {
-		return s == x.boolsgn() && m == x.mantissa();
+		return sign == x.boolsgn() && mant == x.mantissa();
 	}
 
 	/* Checks if this has the value NOT equal to that of an object. */
@@ -152,16 +166,16 @@ public:
 
 	/* Checks if this has a value greater than that of an object. */
 	const bool operator>(const BigInteger& x) const {
-		if (s != x.boolsgn())
+		if (sign != x.boolsgn())
 			return x.boolsgn();
 		else if (size() != x.size())
-			return (size() > x.size()) != s;
+			return (size() > x.size()) != sign;
 		else {
 			size_t t = size();
 			while (t) {
 				--t;
 				if (operator[](t) > x[t])
-					return!s;
+					return!sign;
 			}
 			return false;
 		}
@@ -184,16 +198,16 @@ public:
 
 	/* Takes the value of bitwise AND operation. */
 	BigInteger& operator&=(const BigInteger& x) {
-		s = s && x.boolsgn();
+		sign = sign && x.boolsgn();
 		const size_t minn = MIN(size(), x.size());
 		for (
 			size_t t = 0;
 			t < minn;
 			t++
 			)
-			m[t] &= x[t];
+			mant[t] &= x[t];
 		if (size() > x.size())
-			m.resize(x.size());
+			mant.resize(x.size());
 		return*this;
 	}
 
@@ -205,7 +219,7 @@ public:
 
 	/* Takes the value of bitwise OR operation. */
 	BigInteger& operator|=(const BigInteger& x) {
-		s = s || x.boolsgn();
+		sign = sign || x.boolsgn();
 		const size_t minn = MIN(size(), x.size());
 		const auto xn = x.size();
 		for (
@@ -213,14 +227,14 @@ public:
 			t < minn;
 			t++
 			)
-			m[t] |= x[t];
+			mant[t] |= x[t];
 		if (size() < xn)
 			for (
 				size_t t = size();
 				t < xn;
 				t++
 				)
-				m.push_back(x[t]);
+				mant.push_back(x[t]);
 		return*this;
 	}
 
@@ -232,7 +246,7 @@ public:
 
 	/* Takes the value of bitwise XOR operation. */
 	BigInteger& operator^=(const BigInteger& x) {
-		s = s != x.boolsgn();
+		sign = sign != x.boolsgn();
 		const size_t minn = MIN(size(), x.size());
 		const auto xn = x.size();
 		for (
@@ -240,14 +254,14 @@ public:
 			t < minn;
 			t++
 			)
-			m[t] ^= x[t];
+			mant[t] ^= x[t];
 		if (size() < xn)
 			for (
 				size_t t = size();
 				t < xn;
 				t++
 				)
-				m.push_back(x[t]);
+				mant.push_back(x[t]);
 		return*this;
 	}
 
@@ -262,15 +276,57 @@ public:
 	template<typename xEZ>
 	BigInteger& operator<<=(const xEZ& x) {
 		const size_t
-			p = ABS(x),
-			d = p / ubitn,
-			n = size();
+			a = ABS(x),
+			b = sizeof(unitt) * 8,
+			adb = a / b,
+			n = size(),
+			amb = a % b,
+			bamb = b - amb;
+		size_t t = 0;
+		mant.resize(n + 1);
 		if (x < 0) {
-			//TODO
+			if (amb) {
+				for (t = 0; t < n - adb; t++) {
+					unitt
+						& madbt = mant[adb + t],
+						& madbt1 = mant[adb + t + 1];
+					unitt temp = (madbt1 << bamb) | ((madbt >> amb) & ((1 << bamb) - 1));
+					madbt1 &= -(1 << amb);
+					madbt &= ((1 << amb) - 1);
+					mant[t] = temp;
+				}
+				mant.resize(n - adb + 1);
+			}
+			else {
+				for (t = 0; t < n - adb; t++) {
+					unitt& madbt = mant[adb + t];
+					mant[t] = madbt;
+					madbt = 0;
+				}
+				mant.resize(n - adb);
+			}
 		}
 		else if (!x);
 		else {
-			//TODO
+			mant.resize(n + adb + 1);
+			if (amb) {
+				for (t = 0; t < n; t++) {
+					unitt
+						& mzadbt = mant[n - 1 + adb - t],
+						& mzadbt1 = mant[n - 1 + adb - t - 1];
+					unitt temp = (mzadbt << amb) | ((mzadbt1 >> bamb) & ((1 << amb) - 1));
+					mzadbt &= -(1 << bamb);
+					mzadbt1 &= ((1 << bamb) - 1);
+					mant[n - 1 - t] = temp;
+				}
+			}
+			else {
+				for (t = 0; t < n; t++) {
+					unitt& mzadbt = mant[n - 1 + adb - t];
+					mant[n - 1 - t] = mzadbt;
+					mzadbt = 0;
+				}
+			}
 		}
 		shorten();
 		return*this;
@@ -288,25 +344,5 @@ public:
 	BigInteger operator>>(const xEZ& x) const {
 		BigInteger t = *this;
 		return t >>= x;
-	}
-
-	/* Returns a hexadecimal string of this. */
-	const std::string _16(bool specified = false)const {
-		std::string out;
-		const size_t nx = sizeof(unitt) * 2;
-		char temp[6], * tempp = new char[nx + 1 + 2 + 1];
-		sprintf_s(tempp, nx + 1 + 2 + 1, "%s%s%X", s ? "-" : "", specified ? "0x" : "", m.back());
-		out += tempp;
-		for (
-			intmax_t t = size() - 2;
-			t >= 0;
-			t--
-			) {
-			sprintf_s(temp, 6, "%%0%zuX", nx);
-			sprintf_s(tempp, nx + 1, temp, m[t]);
-			out += tempp;
-		}
-		delete[]tempp;
-		return out;
 	}
 };
--- NearNumber/main.cpp
+++ NearNumber/main.cpp
@@ -1,10 +1,7 @@
 #include<iostream>
 #include"NearNumber.h"
 int main(void) {
-	BigInteger x, y;
-	x = 0x0fedcba987654321;
-	y = 0x7015bc17293abd80;
-	y[1] = 0;
-	std::cout << (y.size()) << std::endl;
+	BigInteger x = 0x0123456789abcdef;
+	std::cout << (x >>20)._16(true) << std::endl;
 	return 0;
 }

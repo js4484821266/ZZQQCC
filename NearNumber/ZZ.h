8d94892cfec30be5cfb8c41056bdf1adea6fd0c0
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-07-10 23:32:38 +0900

    some caption added

--- NearNumber/ZZ.h
+++ NearNumber/ZZ.h
@@ -8,8 +8,18 @@
 #define ABS(x) ((x)<0?-(x):(x))
 //An abbreviation of "integer-integer unit type."
 typedef unsigned int iiunitt;
+//A class for addition and its inverse in base 2**(sizeof(iiunitt)*8)
 class idigits :public std::vector<iiunitt> {
 public:
+	//Reduces length of this so that save more space.
+	void shorten(void) {
+		while (size() > 1)
+			if (!back())
+				pop_back();
+			else
+				break;
+	}
+	//Adds x to this.
 	idigits& addz(const idigits& x) {
 		const auto
 			b = sizeof(iiunitt) * 8,
@@ -44,8 +54,10 @@ public:
 				}
 			}
 		}
+		shorten();
 		return*this;
 	}
+	//Checks if x is greater than this.
 	bool operator<(const idigits& x)const {
 		if (size() != x.size())
 			return size() < x.size();
@@ -61,7 +73,8 @@ public:
 			return false;
 		}
 	}
-	idigits& subz(idigits x) {
+	//Takes difference of this and x.
+	idigits& difz(idigits x) {
 		const auto
 			n = size(),
 			xn = x.size();
@@ -78,43 +91,83 @@ public:
 				idigits i;
 				i.resize(t + 1);
 				i.push_back(1);
-				subz(i);
+				difz(i);
 			}
 			at(t) -= x.at(t);
 		}
+		shorten();
 		return*this;
 	}
 };
 
+/*
+'ZZ' is an extended integer class.
+This class has been named as 'ZZ' because it is to represent more integers,
+and 'ZZ' seems like the blackboard bold letter of Z.
+*/
 class ZZ {
+private:
 	idigits mant;
-	void shorten(void) {
-		while (mant.size() > 1)
-			if (!mant.back())
-				mant.pop_back();
-			else
-				break;
-	}
 public:
+	//Boolean sign.
 	bool sign = 0;
 
-	ZZ operator-()const {
-		ZZ t = *this;
-		t.sign = !t.sign;
-		return t;
-	}
-
-	/* Returns a reference to a vector (a flexible array) of mantissa. */
+	//Returns a reference to the mantissa.
 	const idigits& mantissa(void) const {
 		return mant;
 	}
 
-	/* Returns size of the mantissa vector. */
+	//Returns the number of elements to represent the integer.
 	const size_t size(void)const {
 		return mant.size();
 	}
 
-	/* Returns a hexadecimal string of this. */
+	//Returns a constant reference to a specified element of the mantissa.
+	const iiunitt& operator[](const size_t& index) const {
+		return mant.at(index);
+	}
+	//Returns a reference to a specified element of the mantissa.
+	iiunitt& operator[](const size_t& index) {
+		return mant.at(index);
+	}
+
+	//Returns SGN function.
+	const intmax_t sgn(void) const {
+		if (size() == 1 && !mant.at(0))
+			return 0;
+		else
+			if (sign)
+				return-1;
+			else
+				return 1;
+	}
+
+	//Checks if this is 0.
+	const bool operator!()const {
+		return (size() == 1 && !mant.at(0));
+	}
+
+	//Checks if this and x are both non-zero.
+	const bool operator&&(const ZZ& x)const {
+		return!(operator!() || !x);
+	}
+
+	//Checks if this or x is non-zero.
+	const bool operator||(const ZZ& x) const {
+		return!(operator!() && !x);
+	}
+
+	//Checks if this is equal to x.
+	const bool operator==(const ZZ& x)const {
+		return sign == x.sign && mant == x.mantissa();
+	}
+
+	//Checks if this differs from x.
+	const bool operator!=(const ZZ& x)const {
+		return!operator==(x);
+	}
+
+	//Returns a string representing this in base 16.
 	const std::string hexadec(bool prefix = false)const {
 		std::string out;
 		const auto n = size();
@@ -135,20 +188,35 @@ public:
 		return out;
 	}
 
-	/* Returns a unit of the mantissa vector. */
-	const iiunitt& operator[](const size_t& index) const {
-		return mant.at(index);
+	const bool operator>(const ZZ& x) const {
+		if (sign != x.sign)
+			return x.sign;
+		else if (size() != x.size())
+			return (size() > x.size()) != sign;
+		else {
+			size_t t = size();
+			while (t) {
+				--t;
+				if (operator[](t) > x[t])
+					return!sign;
+			}
+			return false;
+		}
 	}
-	iiunitt& operator[](const size_t& index) {
-		return mant.at(index);
+	const bool operator>=(const ZZ& x)const {
+		return operator>(x) || operator==(x);
+	}
+	const bool operator<(const ZZ& x) const {
+		return !operator>=(x);
+	}
+	const bool operator<=(const ZZ& x)const {
+		return !operator>(x);
 	}
 
-	/* Returns a bit of the mantissa vector in boolean form. */
 	const bool bit(const size_t& index) const {
 		return!!(mant.at(index / (8 * sizeof(iiunitt))) & (1 << (index % (8 * sizeof(iiunitt)))));
 	}
 
-	/* Initialises this to a copy of an object. */
 	const ZZ& operator=(const ZZ& x) {
 		sign = x.sign;
 		mant = x.mantissa();
@@ -158,7 +226,6 @@ public:
 		sign(x.sign),
 		mant(x.mantissa()) {  }
 
-	/* Initialises this to an integer. */
 	const intmax_t& operator=(const intmax_t& x) {
 		const size_t nbyte = MAX(sizeof(intmax_t) / sizeof(iiunitt), 1);
 		const uintmax_t a = ABS(x);
@@ -175,77 +242,45 @@ public:
 					a >> (b * t)
 				)
 			);
-		shorten();
+		mant.shorten();
 		return x;
 	}
 	ZZ(const intmax_t& x = 0) { operator=(x); }
 
-	/* Returns arithmetic sign: -1 if negative, 1 if positive, and 0 otherwise. */
-	const intmax_t sgn(void) const {
-		if (size() == 1 && !mant.at(0))
-			return 0;
-		else
-			if (sign)
-				return-1;
-			else
-				return 1;
+	ZZ abs(void)const {
+		ZZ t = *this;
+		t.sign = false;
+		return t;
 	}
 
-	/* Checks if 0. */
-	const bool operator!()const {
-		return (size() == 1 && !mant.at(0));
+	ZZ operator-()const {
+		ZZ t = *this;
+		t.sign = !t.sign;
+		return t;
 	}
 
-	/* Performs a logical AND comparison with an object. */
-	const bool operator&&(const ZZ& x)const {
-		return!(operator!() || !x);
-	}
-
-	/* Performs a logical OR comparison with an object. */
-	const bool operator||(const ZZ& x) const {
-		return!(operator!() && !x);
-	}
-
-	/* Checks if this has the value equal to that of an object. */
-	const bool operator==(const ZZ& x)const {
-		return sign == x.sign && mant == x.mantissa();
-	}
-
-	/* Checks if this has the value NOT equal to that of an object. */
-	const bool operator!=(const ZZ& x)const {
-		return!operator==(x);
-	}
-
-	/* Checks if this has a value greater than that of an object. */
-	const bool operator>(const ZZ& x) const {
-		if (sign != x.sign)
-			return x.sign;
-		else if (size() != x.size())
-			return (size() > x.size()) != sign;
+	ZZ& operator+=(const ZZ& x) {
+		bool xsign = x.sign;
+		if (sign == xsign)
+			mant.addz(x.mantissa());
 		else {
-			size_t t = size();
-			while (t) {
-				--t;
-				if (operator[](t) > x[t])
-					return!sign;
-			}
-			return false;
+			mant.difz(x.mantissa());
+			if (abs() < x.abs())
+				sign = xsign;
 		}
+		mant.shorten();
+		return*this;
 	}
-
-	/* Checks if this has a value greater than or equal to that of an object. */
-	const bool operator>=(const ZZ& x)const {
-		return operator>(x) || operator==(x);
+	ZZ operator+(const ZZ& x)const {
+		ZZ t = *this;
+		return t += x;
 	}
-
-	/* Checks if this has a value less than that of an object. */
-	const bool operator<(const ZZ& x) const {
-		return !operator>=(x);
+	ZZ& operator-=(const ZZ& x) {
+		return operator+=(-x);
 	}
-
-	/* Checks if this has a value less than or equal to that of an object. */
-	const bool operator<=(const ZZ& x)const {
-		return !operator>(x);
+	ZZ operator-(const ZZ& x)const {
+		ZZ t = *this;
+		return t -= x;
 	}
 
 	/* Takes the value of bitwise AND operation. */
@@ -260,10 +295,9 @@ public:
 			mant[t] &= x[t];
 		if (size() > x.size())
 			mant.resize(x.size());
-		shorten();
+		mant.shorten();
 		return*this;
 	}
-
 	/* Returns the value of bitwise AND operation. */
 	ZZ operator&(const ZZ& x)  const {
 		ZZ t = *this;
@@ -288,10 +322,9 @@ public:
 				t++
 				)
 				mant.push_back(x[t]);
-		shorten();
+		mant.shorten();
 		return*this;
 	}
-
 	/* Returns the value of bitwise OR operation. */
 	ZZ operator|(const ZZ& x) const {
 		ZZ t = *this;
@@ -316,10 +349,9 @@ public:
 				t++
 				)
 				mant.push_back(x[t]);
-		shorten();
+		mant.shorten();
 		return*this;
 	}
-
 	/* Returns the value of bitwise XOR operation. */
 	ZZ operator^(const ZZ& x) const {
 		ZZ t = *this;
@@ -380,7 +412,7 @@ public:
 				}
 			}
 		}
-		shorten();
+		mant.shorten();
 		return*this;
 	}
 	ZZ& operator>>=(const int& x) {
@@ -393,32 +425,6 @@ public:
 	ZZ operator>>(const int& x) const {
 		ZZ t = *this;
 		return t >>= x;
-	}
-
-	/* Takes the value of addition. */
-	ZZ& operator+=(ZZ x) {
-		bool xsgn = x.sign;
-		if (sign == xsgn)
-			mant.addz(x.mantissa());
-		else {
-			mant.subz(x.mantissa());
-			x.sign = 0;
-			if (operator<(x))
-				sign = xsgn;
-		}
-		shorten();
-		return*this;
-	}
-	ZZ operator+(const ZZ& x)const {
-		ZZ t = *this;
-		return t += x;
-	}
-	ZZ& operator-=(const ZZ& x) {
-		return operator+=(-x);
-	}
-	ZZ operator-(const ZZ& x)const {
-		ZZ t = *this;
-		return t -= x;
 	}
 
 	ZZ& operator*=(const ZZ& x) {
@@ -445,11 +451,42 @@ public:
 			it++;
 		}
 		sign = sign != x.sign;
-		shorten();
+		mant.shorten();
 		return*this;
 	}
 	ZZ operator*(const ZZ& x)const {
 		ZZ t = *this;
 		return t *= x;
+	}
+	ZZ& QR(const ZZ& x, ZZ& quotient) {
+		quotient = 0;
+		if (!x || operator!());
+		else {
+			const size_t b = sizeof(iiunitt) * 8;
+			while (operator>=(x)) {
+				size_t
+					dn0 = b - 1,
+					dnx = b - 1;
+				while (!(mant.back() & (1 << dn0)))
+					dn0--;
+				while (!(x.mantissa().back() & (1 << dnx)))
+					dnx--;
+				dn0 += (size() - 1) * b;
+				dnx += (x.size() - 1) * b;
+				int temp = dn0 - dnx;
+				if (*this < x << temp)
+					temp--;
+				quotient |= ZZ(1) << temp;
+				mant.difz((x << temp).mantissa());
+				mant.shorten();
+			}
+		}
+		sign = quotient.sign = sign != x.sign;
+		if (sign && !x.sign) {
+			quotient -= 1;
+			operator+=(x);
+		}
+		mant.shorten();
+		return*this;
 	}
 };

5772e8451e345b2934b4982526476fe443191d22
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-07-17 22:21:43 +0900

    add a function turning it into a usual integer

--- NearNumber/ZZ.h
+++ NearNumber/ZZ.h
@@ -130,6 +130,9 @@ public:
 	//Returns SGN function.
 	const intmax_t sgn(void) const;
 
+	//Returns this as a usual integer.
+	intmax_t operator+();
+
 	//Checks if this is 0.
 	const bool operator!()const;
 
@@ -252,6 +255,18 @@ const intmax_t ZZ::sgn(void) const {
 			return-1;
 		else
 			return 1;
+}
+intmax_t ZZ::operator+() {
+	const auto bb = MAX(1, sizeof(intmax_t) / sizeof(iiunitt));
+	auto t = bb;
+	intmax_t buf = 0;
+	for (
+		t = 0;
+		t < bb;
+		t++
+		)
+		buf |= intmax_t(mant.at(t)) << sizeof(iiunitt) * 8;
+	return buf * sgn();
 }
 const bool ZZ::operator!()const {
 	return (size() == 1 && !mant.at(0));
